539b898e234cc6ba1576ce16c73b0bbeab4c7e44
ChoiMiSeul <73466635+ChoiMiSeul@users.noreply.github.com>
2021-07-09 02:54:25 +0900

    210709

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,13 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); //memberRepository 인터페이스를 구현할 구현체로 MMR 사용
+//    private final MemberRepository memberRepository = new MemoryMemberRepository(); //memberRepository 인터페이스를 구현할 구현체로 MMR 사용
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     @Override
     public void join(Member member) {
