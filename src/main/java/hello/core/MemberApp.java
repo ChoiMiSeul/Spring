d70dfce5b833ff88d8dbe2ecfb7572632a1f5a34
ChoiMiSeul <73466635+ChoiMiSeul@users.noreply.github.com>
2021-07-12 19:34:20 +0900

    210712

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,17 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //객체들의 모든 것 관리. appconfig에 있는 환경 설정 정보를 가지고(@Bean) 스프링에 생성해 관리
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);//config에서 꺼낼 메소드의 이름, 반환타입
+
 //        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
