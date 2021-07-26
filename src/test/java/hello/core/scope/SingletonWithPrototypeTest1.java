c3a5cc087173b1425359e67360e4e6146a347b07
ChoiMiSeul <73466635+ChoiMiSeul@users.noreply.github.com>
2021-07-26 23:17:23 +0900

    210726 Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -36,21 +38,30 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
         // 그래서 2가 나오는 이유는 프로토타입빈은 생성 시점에 주입되었기 때문에 그 빈을 계속 사용해서 count가 2가 되는 것?
         // 아님. p.bean이 생성은 되지만 싱글톤과 함께 유지되기 때문에 2가 되는 것
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성 시점에 주입
+//        private final PrototypeBean prototypeBean; //생성 시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        //getObject 호출시 스프링컨테이너에서 pb를 찾아서 꺼내 반환해줌
+        //단순한 기능(DL-의존관계 조회)으로 각각 다른 객체 생성 가능
+        //ObjectFactory는 getObject만 제공. provider는 f를 상속받아 여러 기능 추가
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); //.getObject();
+
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
